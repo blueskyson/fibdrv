cad6244ef8147d850e45983d1a733cc6e1266649
blueskyson <blueskyson1401@gmail.com>
2022-03-22 03:10:23 +0800

    Finish first version of fast doubling
    The implementation will get error when computiing Fib 186. Require
    fix.

--- fibdrv.c
+++ fibdrv.c
@@ -26,10 +26,14 @@ static struct class *fib_class;
 static DEFINE_MUTEX(fib_mutex);
 
 /* replace unsigned long long with struct BigN */
+#define BIGN
 #define BIGN_ADDING
+//#define BIGN_FAST_DOUBLING
 #define BIGNSIZE 12
 #define BUFFSIZE 500
 
+
+#ifdef BIGN
 typedef struct BigN {
     unsigned long long cell[BIGNSIZE];
 } ubig;
@@ -48,20 +52,6 @@ static inline void ubig_add(ubig *dest, ubig *a, ubig *b)
     for (int i = 0; i < BIGNSIZE - 1; i++)
         dest->cell[i + 1] += (dest->cell[i] < a->cell[i]);
 }
-
-// void ubig_lshift(ubig *dest, ubig *a, int x)
-// {
-//     // quotient and remainder of being divided by 64
-//     unsigned quotient = x >> 6, remainder = x & 0x3f;
-
-//     init_ubig(dest);
-//     for (int i = 0; i + quotient < BIGNSIZE; i++)
-//         dest->cell[i + quotient] |= a->cell[i] << remainder;
-
-//     if (remainder)
-//         for (int i = 1; i + quotient < BIGNSIZE; i++)
-//             dest->cell[i + quotient] |= a->cell[i - 1] >> (64 - remainder);
-// }
 
 int ubig_to_string(char *buf, size_t bufsize, ubig *a)
 {
@@ -92,6 +82,7 @@ int ubig_to_string(char *buf, size_t bufsize, ubig *a)
         offset++;
     return (buf[offset] == '\0') ? (offset - 1) : offset;
 }
+#endif
 
 
 #ifdef BIGN_ADDING
@@ -121,7 +112,87 @@ static ubig fib_sequence_ubig(long long k)
 
 #elif defined BIGN_FAST_DOUBLING
 
-/* code */
+static inline void ubig_sub(ubig *dest, ubig *a, ubig *b)
+{
+    for (int i = 0; i < BIGNSIZE; i++)
+        dest->cell[i] = a->cell[i] - b->cell[i];
+
+    for (int i = 0; i < BIGNSIZE - 1; i++)
+        dest->cell[i + 1] -= (dest->cell[i] > a->cell[i]);
+}
+
+static inline void ubig_lshift(ubig *dest, ubig *a, int x)
+{
+    // quotient and remainder of x being divided by 64
+    unsigned quotient = x >> 6, remainder = x & 0x3f;
+
+    init_ubig(dest);
+    for (int i = 0; i + quotient < BIGNSIZE; i++)
+        dest->cell[i + quotient] |= a->cell[i] << remainder;
+
+    if (remainder)
+        for (int i = 1; i + quotient < BIGNSIZE; i++)
+            dest->cell[i + quotient] |= a->cell[i - 1] >> (64 - remainder);
+}
+
+void ubig_mul(ubig *dest, ubig *a, ubig *b)
+{
+    init_ubig(dest);
+    int index = BIGNSIZE - 1;
+    while (!b->cell[index])
+        index--;
+    if (index == -1)
+        return;
+
+    for (int i = index; i >= 0; i--) {
+        int bit_index = (index << 6) + 63;
+        for (unsigned long long mask = 0x8000000000000000ULL; mask;
+             mask >>= 1) {
+            if (b->cell[i] & mask) {
+                ubig shift, tmp;
+                ubig_lshift(&shift, a, bit_index);
+                ubig_add(&tmp, dest, &shift);
+                *dest = tmp;
+            }
+            bit_index--;
+        }
+    }
+}
+
+static ubig fib_sequence_ubig(long long k)
+{
+    if (k <= 1LL) {
+        ubig ret;
+        init_ubig(&ret);
+        ret.cell[0] = (unsigned long long) k;
+        return ret;
+    }
+
+    ubig a, b;
+    init_ubig(&a);
+    init_ubig(&b);
+    b.cell[0] = 1ULL;
+
+    for (unsigned long long mask = 0x8000000000000000ULL >> __builtin_clzll(k);
+         mask; mask >>= 1) {
+        ubig tmp1, tmp2, t1, t2;
+        ubig_lshift(&tmp1, &b, 1);   // tmp1 = 2*b
+        ubig_sub(&tmp2, &tmp1, &a);  // tmp2 = 2*b - a
+        ubig_mul(&t1, &a, &tmp2);    // t1 = a*(2*b - a)
+
+        ubig_mul(&tmp1, &a, &a);      // tmp1 = a^2
+        ubig_mul(&tmp2, &b, &b);      // tmp2 = b^2
+        ubig_add(&t2, &tmp1, &tmp2);  // t2 = a^2 + b^2
+
+        a = t1, b = t2;
+        if (k & mask) {
+            ubig_add(&t1, &a, &b);  // t1 = a + b
+            a = b, b = t1;
+        }
+    }
+
+    return a;
+}
 
 #else
 static long long fib_sequence(long long k)
@@ -161,14 +232,12 @@ static ssize_t fib_read(struct file *file,
                         size_t size,
                         loff_t *offset)
 {
-#ifdef BIGN_ADDING
+#ifdef BIGN
     char fibbuf[BUFFSIZE];
     ubig fib = fib_sequence_ubig(*offset);
     int __offset = ubig_to_string(fibbuf, BUFFSIZE, &fib);
     copy_to_user(buf, fibbuf + __offset, BUFFSIZE - __offset);
     return (ssize_t) BUFFSIZE - __offset;
-#elif defined BIGN_FAST_DOUBLING
-    return 0;
 #else
     return (ssize_t) fib_sequence(*offset);
 #endif
